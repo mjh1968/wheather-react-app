0d5ae42cc84cdecf9fb65cf9916fa9b4a124587d
mjh1968 <mjbcbh@gmail.com>
2021-05-26 15:25:58 +0100

    add Github

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Wheather App</div>;
+  return (
+    <div className="App">
+      Wheather App
+      <footer>
+        This project was coded by Maria João Hortega
+        <a
+          href="https://github.com/mjh1968/wheather-react-app"
+          targert="_blank"
+        >
+          {" "}
+          and is open sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
